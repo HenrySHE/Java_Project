476992168ee58ad2a79e678a7edc6a03ed7a8252
410338460@qq.com <410338460@qq.com>
2019-12-11 14:21:12 +0800

    UpdateCode

--- hello/hello.java
+++ hello/hello.java
@@ -1,5 +1,10 @@
 public class hello {
     public static void main(String []args) {
-        System.out.println("Hello"); 
+        System.out.println("Hello");
+        List<List<String>> a = new ArrayList<>();
+        List<String> tempList = new ArrayList<>();
+        tempList.add("hello");
+        a.add(tempList);
+        System.out.println(a);
     }
 }
